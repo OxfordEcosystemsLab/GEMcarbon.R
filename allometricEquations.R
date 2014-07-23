b07b3735e453742f7b25713e4e7d3e2888220ac3
Cecile <cecilegirardin@gmail.com>
2014-07-23 23:12:02 +0100

    NPPacw function and data cleaning July 2014
    I wrote new code for cleaning census data. This is not a function, you
    need to run the data through it step by step. I re-wrote the code for
    the NPPacw function. Allometric equations remain the same, just added
    Chave 2014.

--- allometricEquations.R
+++ allometricEquations.R
@@ -1,28 +1,26 @@
-### This script is to define a set of allometric equations that can be called from
-## various pieces of the Carbon-Use-Efficiency code.
+### This script is to define a set of allometric equations that can be called from various functions of the GEM code.
+# Sebastian Sippel 15.02.2014
 
-# Sebastian Sippel, 15.02.2014
-
-## Chave et al, 2005 gives different allometric models to estimate aboveground biomass (AGB),
-# three of which will be used in the R-Code:
-
-# p. 91, Chave et al, 2005: model I.3; dry: 
+## Chave et al, 2005 gives different allometric models to estimate aboveground biomass (AGB), three of which will be used in the R-Code:
+# p. 91, Chave et al, 2005: model I.3 dry: 
 Chave2005_dry <- function(diax, density, height) {
   AGB_est <- 0.112*(density*((diax)^2)*height)^0.916
   return(AGB_est)
 }
 
-# p. 90 and p. 95, Chave et al, 2005: model I.6; the 'simplest' model:
+# p. 90, Chave et al, 2005: model I.6 moist:
 Chave2005_moist <- function(diax, density, height) {
   AGB_est <- 0.0509*density*((diax)^2)*height
   return(AGB_est)
 }
 
+# p. 95, Chave et al, 2005: model I.6 wet:
 Chave2005_wet <- function(diax, density, height) {
   AGB_est <- 0.0776*(density*((diax)^2)*height)^0.940
   return(AGB_est)
 }
 
+# Chave et al, 2014:
 Chave2014 <- function(diax, density, height) {
   AGB_est <- 0.0673*(density*((diax)^2)*height)^0.976 
   return(AGB_est)
